ebe71e320fedd1678bdd91f04d72d63b697a588a
Nicolás Bores <153894747+Nick0590@users.noreply.github.com>
2023-12-15 12:03:09 -0300

    catssql.sql

--- catssql.sql
+++ catssql.sql
@@ -4,10 +4,12 @@ CREATE TEMPORARY TABLE cats_temp AS
 SELECT animal_id, prey_p_month, animal_sex
    FROM cats_uk_reference;
 
+/* Presas por sexo */
 SELECT animal_sex, AVG(prey_p_month) as avg_prey_per_month
    FROM cats_temp
 GROUP BY animal_sex;
 
+/* Join por subconjuntos */
 DROP TEMPORARY TABLE IF EXISTS temp_hunting_data;
 CREATE TEMPORARY TABLE temp_hunting_data AS
    SELECT animal_id, prey_p_month
@@ -22,6 +24,8 @@ SELECT thd.animal_id, thd.prey_p_month, tid.hrs_indoors
    FROM temp_hunting_data thd
 JOIN temp_indoor_data tid ON thd.animal_id = tid.animal_id;
 
+
+/* Caza según animal */
 SELECT animal_id,
        prey_p_month,
        AVG(prey_p_month) OVER (PARTITION BY animal_sex) as avg_prey_by_sex
